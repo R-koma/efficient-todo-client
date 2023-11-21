3f5d1684fc0c8c0609c75ef05962be15dbae2854
R-koma <ryomakomazawa@gmail.com>
2023-11-21 14:20:58 +0900

    レスポンシブデザインを適用

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import ReviewPage from "./pages/ReviewPage";
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-center items-start space-x-4">
-      <div className="w-1/2">
+    <div className="flex flex-col md:flex-row justify-center items-center space-x-4">
+      <div className="w-full md:w-2/3 xl:w-3/4 max-w-4xl">
         <TodoPage />
       </div>
-      <div className="w-1/2">
+      <div className="w-full md:w-2/3 xl:w-3/4 max-w-4xl">
         <ReviewPage />
       </div>
     </div>
